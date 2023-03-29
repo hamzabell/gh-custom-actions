f6b17728d8df86fa740c07c7a1e830e6abb57b26
basseyna <112563812+basseyna@users.noreply.github.com>
2023-03-29 15:01:08 +0100

    added output

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -6,7 +6,11 @@ const exec = require('@actions/exec')
 function run() {
     const name = core.getInput('name', { required: true });
     const age = core.getInput('age');  
+
+    const canDate = parseInt(age) > 30 ? 'Yes' : 'No';
     core.notice(`Hello ${name}, I understand that you are ${age} years old!`);
+    core.setOutput('candate', canDate)
+
 }
 
 run();
